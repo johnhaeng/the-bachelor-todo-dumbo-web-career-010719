3e487aa46a7106c5bdcab37676dcb4678ed9c0b2
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-19 04:46:43 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,18 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = nil 
+
+  data.each do |seasons, info|
+    if seasons == season.to_sym
+      info.each do |contestant|
+        if contestant[:status] == "Winner"
+          winner = contestant[:name].split[0]
+        end 
+      end 
+    end 
+  end 
+  
+  winner 
 end
 
 def get_contestant_name(data, occupation)
